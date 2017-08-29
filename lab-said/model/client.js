4fcd05e5931b1421c6413a65c98700c56938c4c0
saidmattar <ssabdou@gmail.com>
2017-08-28 18:54:48 -0700

    added direct message functionality

--- lab-said/model/client.js
+++ lab-said/model/client.js
@@ -2,8 +2,6 @@
 
 const uuid = require('uuid/v4')
 
-// TODOs: Client needs a nickName, _id, socket
-
 module.exports = function(socket) {
   this.socket = socket
   this.nick = `guest_${Math.random() * 10000}`
--- lab-said/server.js
+++ lab-said/server.js
@@ -20,25 +20,33 @@ ee.on('default', (client, string) => {
 });
 
 server.on('connection', socket => {
- let client = new Client(socket);
- // console.log('client', client)
- pool.push(client);
- pool.forEach(c => c.socket.write(`${client.nick} has joined the channel\n`));
+  let client = new Client(socket);
+  // console.log('client', client)
+  pool.push(client);
+  pool.forEach(c => c.socket.write(`${client.nick} has joined the channel\n`));
 
- socket.on('data', data => {
-   let cmd = data.toString().split(' ').shift().trim();
-   console.log(cmd);
-   if(cmd === '@all') {
-     console.log(data.toString().split(' ').slice(1).join() + '\n');
-     pool.forEach(c => c.socket.write(data.toString().split(' ').slice(1).join() + '\n'));
-   } else if(cmd === '@nick') {
-     client.nick = data.toString().split(' ').pop().trim();
-     console.log('welcom to our room: ' + client.nick );
-   } else if(cmd === '@dm') {
+  socket.on('data', data => {
+    let cmd = data.toString().split(' ').shift().trim();
+    console.log(cmd);
+    if(cmd === '@all') {
+      console.log(data.toString().split(' ').slice(1).join() + '\n');
+      pool.forEach(c => c.socket.write(data.toString().split(' ').slice(1).join() + '\n'));
+    } else if(cmd === '@nick') {
+      client.nick = data.toString().split(' ').pop().trim();
+      console.log('welcom to our room: ' + client.nick );
+    } else if(cmd === '@dm') {
+      let commandLineArr = data.toString().split(' ');
+      console.log(commandLineArr);
+      console.log('the 1 in cLArr', commandLineArr[1]);
+      pool.forEach(client => {
+        if (commandLineArr[1] === client.nick) {
+          console.log(commandLineArr[2]);
+          client.socket.write(commandLineArr[2]);
+        }
 
-   }
-   // console.log(data.toString()) => will print whatever data was transmitted
- });
+      });
+    }
+  });
 });
 
 
