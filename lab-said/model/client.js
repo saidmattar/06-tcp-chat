f093902e504d30f5bd266e9ca059635d62d817ae
saidmattar <ssabdou@gmail.com>
2017-08-28 20:58:56 -0700

    fixed typos

--- lab-said/model/client.js
+++ lab-said/model/client.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const uuid = require('uuid/v4')
+const uuid = require('uuid/v4');
 
 module.exports = function(socket) {
-  this.socket = socket
-  this.nick = `guest_${Math.random() * 10000}`
-  this._id = uuid()
+  this.socket = socket,
+  this.nick = `guest_${Math.random() * 10000}`,
+  this._id = uuid(),
 }
--- lab-said/server.js
+++ lab-said/server.js
@@ -1,19 +1,13 @@
-'use strict'
+'use strict';
 
-// TODO: require in all the modules necessary for server setup
-const net = require('net')
-const EE = require('events').EventEmitter
-const ee = new EE()
-const Client = require('./model/client')
+const net = require('net');
+const EE = require('events').EventEmitter;
+const ee = new EE();
+const Client = require('./model/client');
 
-const server = net.createServer()
+const server = net.createServer();
 
-let pool = []
-
-// TODO: Create the @dm, @nick, @all commands. Anything else should emit a 'default' event
-// @all HEY THERE => this will broadcast to all clients
-// @dm tim HEY THERE => this will broadcast to ONLY tim
-// @nick scott => will change the nickname of this client to scott
+let pool = [];
 
 ee.on('default', (client, string) => {
   client.socket.write(`Improperly formatted command: ${string.split(' ', 1)}\n`);
@@ -21,13 +15,14 @@ ee.on('default', (client, string) => {
 
 server.on('connection', socket => {
   let client = new Client(socket);
-  // console.log('client', client)
+
   pool.push(client);
   pool.forEach(c => c.socket.write(`${client.nick} has joined the channel\n`));
 
   socket.on('data', data => {
     let cmd = data.toString().split(' ').shift().trim();
     console.log(cmd);
+
     if(cmd === '@all') {
       console.log(data.toString().split(' ').slice(1).join() + '\n');
       pool.forEach(c => c.socket.write(data.toString().split(' ').slice(1).join() + '\n'));
@@ -42,12 +37,10 @@ server.on('connection', socket => {
         if (commandLineArr[1] === client.nick) {
           console.log(commandLineArr[2]);
           client.socket.write(commandLineArr[2]);
-        }
-
+        };
       });
-    }
+    };
   });
 });
 
-
-server.listen(3000, () => console.log('listening on port 3000'))
+server.listen(3000, () => console.log('listening on port 3000'));
